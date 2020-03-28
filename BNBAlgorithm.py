1e56f7ea18a12247a22d711ef7fa345b0633044d
rafalb8 <rafalb8@hotmail.com>
2020-03-28 11:23:22 +0200

    Fix bug

--- BNBAlgorithm.py
+++ BNBAlgorithm.py
@@ -5,6 +5,7 @@ class Item:
     def __init__(self, w: float, v: int):
         self.weight = w
         self.value = v
+
     def __str__(self):
         return '({0}, {1})'.format(self.weight, self.value)
 
@@ -13,29 +14,32 @@ class Node:
     def __init__(self, l, p, w, b):
         self.level = l
         self.profit = p
-        self.weight = w
         self.bound = b
+        self.weight = w
+
+    def copy(self):
+        return Node(self.level, self.profit, self.weight, self.bound)
 
     def __str__(self):
         return '{}|{}|{}|{}'.format(self.level, self.profit, self.weight, self.bound)
 
 
-def bound(u : Node, n, W, arr):
+def bound(u: Node, n, W, arr):
     if u.weight >= W:
         return 0
 
     profit_bound = u.profit
 
     j = u.level + 1
-    wt = u.weight
+    tot_weight = u.weight
 
-    while (j < n) and (wt + arr[j].weight <= W):
-        wt += arr[j].weight
+    while (j < n) and (tot_weight + arr[j].weight <= W):
+        tot_weight += arr[j].weight
         profit_bound += arr[j].value
         j += 1
 
     if j < n:
-        profit_bound += (W - wt) * float(arr[j].value) / arr[j].weight
+        profit_bound += (W - tot_weight) * arr[j].value // arr[j].weight
 
     return profit_bound
 
@@ -45,16 +49,17 @@ def knapsack(W, arr, n):
 
     Q = Queue()
 
-    u = Node(-1, p=0, w=0.0, b=0.0)
+    u = Node(l=-1, p=0, w=0.0, b=0)
+    v = Node(None, None, None, None)
     Q.put((u, -1))
 
     max_profit = 0
     while not Q.empty():
-        v, node = Q.get()
-        print(v, node)
+        u, node = Q.get()
+        print(u, node)
 
-        #if u.level == -1:
-        #    v.level = 0
+        if u.level == -1:
+           v.level = 0
 
         if u.level == n - 1:
             continue
@@ -70,20 +75,20 @@ def knapsack(W, arr, n):
         v.bound = bound(v, n, W, arr)
 
         if v.bound > max_profit:
-            Q.put((v, arr[v.level]))
+            Q.put((v.copy(), arr[v.level]))
 
         v.weight = u.weight
         v.profit = u.profit
         v.bound = bound(v, n, W, arr)
         if v.bound > max_profit:
-            Q.put((v, arr[v.level]))
+            Q.put((v.copy(), arr[v.level]))
 
     return max_profit
 
 
 W = 10
 
-arr = [Item(2, 40), Item(3.14, 50), Item(1.98, 100), Item(5, 95), Item(3, 30)]
+arr = [Item(2.0, 40), Item(3.14, 50), Item(1.98, 100), Item(5.0, 95), Item(3.0, 30)]
 
 n = 5
 
