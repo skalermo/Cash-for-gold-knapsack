bf333b2bd234f68c2ca8c2ee7d1cc7e40b2241bf
skalermo <skalermo@gmail.com>
2020-03-29 14:31:10 +0200

    Implement generation with custom correlation

--- Generator.py
+++ Generator.py
@@ -1,10 +1,11 @@
+import numpy as np
 import random
 
 
 def gen_data(n: int = 10,
-             v: int = 10,
-             r: int = 5,
-             correlation: str = 'weak',
+             w_cap: int = 10,
+             p_cap: int = 10,
+             correlation: float = 0.5,
              capacity: float = None,
              capacity_type: str = 'restrictive',
              seed: int = 0
@@ -13,18 +14,9 @@ def gen_data(n: int = 10,
     Generate data according to formulas:
     (W stands for weights, P - for profits, C - for capacity)
 
-    correlation: none
-        W[i] := (uniformly) random(1..v)
-        P[i] := (uniformly) random(1..v)
-
-    correlation: weak
-        W[i] := (uniformly) random(1..v)
-        P[i] := W[i] + (uniformly) random(-r..r)
-        if P[i] < 0: repeat prev line until P[i] > 0
-
-    correlation: strong
-        W[i] := (uniformly) random(1..v)
-        P[i] := W[i] + r
+    With approximated correlation
+        W[i] := (uniformly) random(0..v)
+        P[i] := (uniformly) random(0..v)
 
     capacity_type: restrictive
         C = 2v
@@ -36,31 +28,29 @@ def gen_data(n: int = 10,
         C = n * avg(W) * random(0..1)
 
     :param n: Number of items
-    :param v: Weight cap
-    :param r: Correlation value
+    :param w_cap: Weight max value
+    :param p_cap: Profit max value
     :param correlation: Correlation between profits and weights
     :param capacity: Set capacity explicitly
-    :param capacity_type: Restrictive, average or random
-    :param seed: Random seed
+    :param capacity_type: Restrictive, average or random if not set explicitly
+    :param seed: Numpy random seed
     :return: Dictionary with generated data
     """
 
-    random.seed(seed)
+    np.random.seed(seed)
 
-    weights = [round(random.uniform(1, v), 1) for _ in range(n)]
+    weights, profits, real_corr_coef = _gen_correlated_uniform_data(n, w_cap, p_cap, correlation)
+    weights = weights.tolist()
+    profits = profits.tolist()
 
-    profits = {
-        'none': lambda: [round(random.uniform(1, v), 1) for _ in range(n)],
-        'weak': lambda: _weak_corr_profits(n, r, weights),
-        'strong': lambda: [round(r + weights[i], 1) for i in range(n)]
-    }.get(correlation, 'weak')()
-
-    if not capacity:
+    if capacity is None:
         capacity = {
-            'restrictive': lambda: 2*v,
+            'restrictive': lambda: 2*w_cap,
             'average': lambda: 0.5*sum(weights),
-            'random': lambda: round(random.random()*n*sum(weights)/len(weights), 1)
+            'random': lambda: round(np.random.rand()*n*sum(weights)/len(weights), 1)
         }.get(capacity_type, 'restrictive')()
+    else:
+        capacity_type = 'custom'
 
     data = {
         'weights': weights,
@@ -68,16 +58,74 @@ def gen_data(n: int = 10,
         'capacity': capacity,
         'capacity_type': capacity_type,
         'correlation': correlation,
+        'real_correlation': real_corr_coef,
         'seed': seed
     }
     return data
 
 
-def _weak_corr_profits(n: int, r: float, weights: list) -> list:
-    profits = []
-    for i in range(n):
-        profit = -1
-        while profit <= 0.0:
-            profit = weights[i] + round(random.uniform(-r, r), 1)
-        profits.append(profit)
-    return profits
+def _gen_correlated_uniform_data(n: int, sx: float, sy: float, corr_coef: float) -> (list, list, float):
+    """
+    Generate data uniformly from (0,sx)x(0,sy) with set correlation coefficient.
+    Based on http://www.acooke.org/random.pdf.
+    X and Y are rounded to 1 decimal place
+    :param n: Number of samples
+    :param sx: X cap
+    :param sy: Y cap
+    :param corr_coef: Positive correlation between x and y.
+    :return: X, Y, real correlation coefficient
+    """
+
+    assert 0 <= corr_coef <= 1
+
+    # adjust correlation coefficient
+    t = 1 - __adjustment_function(corr_coef)
+
+    # generate points from (0,1)x(0,1), (0,t)x(0,1) and (1-t,1)x(0,1)
+    X = np.hstack([np.random.uniform(size=n),
+                   np.random.uniform(low=0, high=t, size=round(t * n)),
+                   np.random.uniform(low=1-t, high=1, size=round(t * n))])
+
+    Y = np.hstack([np.random.uniform(size=n),
+                   np.random.uniform(low=0, high=t, size=round(t * n)),
+                   np.random.uniform(low=1-t, high=1, size=round(t * n))])
+    points = np.array([X, Y])
+
+    # scale by (sqrt(2), t*sqrt(2))
+    scale_matrix = np.array([[np.sqrt(2), 0],
+                             [0, t * np.sqrt(2)]])
+    points = np.dot(scale_matrix, points)
+
+    # translate by vector (0, -t*sqrt(2)/2)
+    points = points - np.array([[0], [t * np.sqrt(2) / 2]])
+
+    # rotate counterclockwise through -45 deg about the origin
+    rotate_matrix = np.array([[np.sqrt(2) / 2, -np.sqrt(2) / 2],
+                              [np.sqrt(2) / 2, np.sqrt(2) / 2]])
+    points = np.dot(rotate_matrix, points)
+
+    # scale by (sx, sy)
+    scale_matrix = np.array([[sx, 0],
+                             [0, sy]])
+    points = np.dot(scale_matrix, points)
+
+    # find points within range (0,1)x(0,1)
+    # it's 0.05 because of round operation later
+    ids = (0.05 < points[0]) & (points[0] < sx) & (0.05 < points[1]) & (points[1] < sy)
+    X = np.round(points[0][ids][:n], 1)
+    Y = np.round(points[1][ids][:n], 1)
+
+    # calc real correlation coefficient
+    real_corr_coef = np.corrcoef(X, Y)[0, 1]
+
+    return X, Y, real_corr_coef
+
+
+def __adjustment_function(x):
+    """
+    Magick function based on Lagrange interpolation.
+    Adjusts correlation coefficient.
+    """
+    return 843.491 * x ** 9 - 3747.36 * x ** 8 + 6991.44 * x ** 7 - \
+        7112.28 * x ** 6 + 4290.12 * x ** 5 - 1566.84 * x ** 4 +\
+        342.07 * x ** 3 - 43.6857 * x ** 2 + 4.04011 * x
