faef7582f5afa5bf80e0033b1bd5b1b4875b3e19
skalermo <skalermo@gmail.com>
2020-03-30 11:50:54 +0200

    Fix setting random seed

--- Generator.py
+++ Generator.py
@@ -1,13 +1,14 @@
 import random
+from datetime import datetime
 
 
-def gen_data(n: int = 10,
+def gen_data(n: int = 100,
              v: int = 10,
              r: int = 5,
              correlation: str = 'weak',
              capacity: float = None,
              capacity_type: str = 'restrictive',
-             seed: int = 0
+             seed: int = None
              ) -> dict:
     """
     Generate data according to formulas:
@@ -45,6 +46,9 @@ def gen_data(n: int = 10,
     :return: Dictionary with generated data
     """
 
+    # set seed
+    if seed is None:
+        seed = datetime.now().microsecond
     random.seed(seed)
 
     weights = [round(random.uniform(1, v), 1) for _ in range(n)]
@@ -55,12 +59,14 @@ def gen_data(n: int = 10,
         'strong': lambda: [round(r + weights[i], 1) for i in range(n)]
     }.get(correlation, 'weak')()
 
-    if not capacity:
+    if capacity is None:
         capacity = {
             'restrictive': lambda: 2*v,
             'average': lambda: 0.5*sum(weights),
             'random': lambda: round(random.random()*n*sum(weights)/len(weights), 1)
         }.get(capacity_type, 'restrictive')()
+    else:
+        capacity_type = 'custom'
 
     data = {
         'weights': weights,
@@ -74,6 +80,13 @@ def gen_data(n: int = 10,
 
 
 def _weak_corr_profits(n: int, r: float, weights: list) -> list:
+    """
+    Generate profits weakly correlated to provided weights
+    :param n: Number of items
+    :param r: Difference cap between weights and profits
+    :param weights: Provided list of weights
+    :return: List of profits
+    """
     profits = []
     for i in range(n):
         profit = -1
