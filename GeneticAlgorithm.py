072b5a8a05166e97d005d179e3905194bcd3306b
rafalb8 <rafalb8@hotmail.com>
2020-03-31 11:28:11 +0300

    Add fitness function

--- GeneticAlgorithm.py
+++ GeneticAlgorithm.py
@@ -1,5 +1,6 @@
 import random
 from datetime import datetime
+import numpy as np
 
 
 def crossover(parents, seed=None, point=None):
@@ -12,9 +13,9 @@ def crossover(parents, seed=None, point=None):
     :param parents: Chromosome pair
     :param seed:    Random seed
     :param point:   Crossover point
-    :return:
+    :return:        2 children
     """
-    chromosome1, chromosome2 = parents
+    x, y = parents
 
     # Set seed
     if seed is None:
@@ -23,14 +24,44 @@ def crossover(parents, seed=None, point=None):
 
     # Choose crossover point
     if point is None:
-        point = random.randint(0, len(chromosome1) - 1)
+        point = random.randint(0, len(x) - 1)
 
     # Cross genes
-    child1 = chromosome1[:point] + chromosome2[point:]
-    child2 = chromosome2[:point] + chromosome1[point:]
+    child1 = x[:point] + y[point:]
+    child2 = y[:point] + x[point:]
 
     return child1, child2
 
+
+def fitness(x, data, has_penalty=False):
+    """
+    Fitness evaluation function with logarithmic penalty function
+
+    :param x:           Solution to the problem
+    :param data:        Dictionary with weights, capacity and profits
+    :param has_penalty: Boolean. If True - use penalty function
+    :return:            Fitness evaluation
+    """
+    penalty = 0
+    if has_penalty:
+        # Find p = max(P[i] / W[i]) and weight_sum = sum(x[i] * W[i] - C)
+        p = 0
+        weight_sum = 0
+        for i in range(len(x)):
+            weight_sum += x[i] * data['weights'][i] - data['capacity']
+
+            ratio = data['profits'][i] / data['weights'][i]
+            if ratio > p:
+                p = ratio
+
+        penalty = np.log2(1 + p * weight_sum)
+
+    # Find fitness = sum(x[i] * P[i] - Penalty(x))
+    fit = 0
+    for i in range(len(x)):
+        fit += x[i] * data['profits'][i] - penalty
+
+    return fit
 
 
 
