8eb8b5b817448fc97f53b31faef32d6263634b6a
skalermo <skalermo@gmail.com>
2020-04-12 17:02:09 +0200

    Add init_population test

--- tests/test_Population.py
+++ tests/test_Population.py
@@ -1,6 +1,7 @@
 import unittest
 from Generator import gen_data
-from GeneticAlgorithm import init_population, fitness
+from GeneticAlgorithm import init_population, fitness, breed_population
+from BNBAlgorithm import knapsack, Item
 
 
 class TestPopulation(unittest.TestCase):
@@ -19,10 +20,18 @@ class TestPopulation(unittest.TestCase):
         self.assertEqual(100, len(population))
 
     def test_init_population_heuristic_solutions(self):
+        hr = 0.3
         data = gen_data()
-        population = init_population(100, data, heuristic_ratio=0.34)
-        n = int(0.34*100)
-        self.assertTrue(all([fitness(solution, data) > 0 for solution in population[:n]]))
+        population = init_population(50, data, heuristic_ratio=hr)
+        first_bits_set_count = 0
+        last_bits_unset_count = 0
+        for chromosome in population:
+            if chromosome[0] == 1:
+                first_bits_set_count += 1
+            if chromosome[-1] == 0:
+                last_bits_unset_count += 1
+        self.assertTrue(first_bits_set_count/50 >= hr)
+        self.assertTrue(last_bits_unset_count/50 >= hr/2)
 
 
 if __name__ == '__main__':
