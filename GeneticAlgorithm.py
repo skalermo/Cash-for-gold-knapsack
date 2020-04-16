9cc4b6e153d628e5f001eeb63c2f0d505ac8d029
rafalb8 <rafalb8@hotmail.com>
2020-04-16 15:03:14 +0300

    Move methods to chromosome class

--- GeneticAlgorithm.py
+++ GeneticAlgorithm.py
@@ -1,62 +1,6 @@
 import random
 from datetime import datetime
-import numpy as np
-
-
-def crossover(parents, seed=None, point=None):
-    """
-    Single point crossover.
-    Produce two offsprings, each carrying some genetic information from both parents.
-
-    (If point is set, seed has no effect on choosing crossover point)
-
-    :param parents: Chromosome pair
-    :param seed:    Random seed
-    :param point:   Crossover point
-    :return:        2 children
-    """
-    x, y = parents
-
-    # Set seed
-    if seed is None:
-        seed = datetime.now().microsecond
-    random.seed(seed)
-
-    # Choose crossover point
-    if point is None:
-        point = random.randint(0, len(x) - 1)
-
-    # Cross genes
-    child1 = x[:point] + y[point:]
-    child2 = y[:point] + x[point:]
-
-    return child1, child2
-
-
-def fitness(x, data, has_penalty=False):
-    """
-    Fitness evaluation function with logarithmic penalty function
-
-    :param x:           Solution to the problem
-    :param data:        Dictionary with weights, capacity and profits
-    :param has_penalty: Boolean. If True - use penalty function
-    :return:            Fitness evaluation
-    """
-    penalty = 0
-    if has_penalty:
-        weight_sum = sum([x[i] * data['weights'][i] for i in range(len(x))])
-        if weight_sum > data['capacity']:
-            # Find p = max(P[i] / W[i])
-            p = max(data['ratios'])
-
-            # Find weight_sum = sum(x[i] * W[i]) - C
-            weight_sum -= data['capacity']
-
-            # Set penalty
-            penalty = np.log2(1 + p * weight_sum)
-
-    # Find fitness = sum(x[i] * P[i] - Penalty(x))
-    return sum([x[i] * data['profits'][i] for i in range(len(x))]) - penalty
+from Chromosome import Chromosome
 
 
 def genetic_algorithm(data, generations, pop_size=100, crossover_rate=0.65, mutation_rate=0.05, seed=None):
@@ -120,7 +64,7 @@ def breed(mating_pool, crossover_rate):
     # breed new and add them to children
     for i in range(len(mating_pool)//2):
         if random.random() < crossover_rate:
-            born_children = crossover(mating_pool[i], mating_pool[-i - 1])
+            born_children = Chromosome.crossover(mating_pool[i], mating_pool[-i - 1])
             children.append(*born_children)
 
     return children
@@ -129,7 +73,7 @@ def breed(mating_pool, crossover_rate):
 def mutate_chromosomes(chromosomes, mutation_rate):
     for chromosome in chromosomes:
         if random.random() < mutation_rate:
-            mutate(chromosome)
+            chromosome.mutate()
 
 
 def init_population(pop_size, data, heuristic_ratio=0):
@@ -164,14 +108,6 @@ def init_population(pop_size, data, heuristic_ratio=0):
     return population
 
 
-def gen_random_chromosome(size) -> list:
-    return [random.randint(0, 1) for _ in range(size)]
+def gen_random_chromosome(size) -> Chromosome:
+    return Chromosome([random.randint(0, 1) for _ in range(size)])
 
-
-def mutate(chromosome):
-    """
-    Select and flip random bit in chromosome
-    :param chromosome: Chromosome to mutate
-    """
-    idx = random.randint(0, len(chromosome)-1)
-    chromosome[idx] = 1 - chromosome[idx]
