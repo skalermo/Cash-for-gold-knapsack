6810d8caef620eea2a3c3e5f82f5974d203514f2
skalermo <skalermo@gmail.com>
2020-05-12 14:55:38 +0200

    Changes: - add few useful functions - update mutation - use itertools.compress instead of multiplying data by bitmap

--- Chromosome.py
+++ Chromosome.py
@@ -1,20 +1,18 @@
 import random
 from datetime import datetime
 import numpy as np
+import itertools
 
 
 class Chromosome:
 
-    def __init__(self, gene, data=None):
+    def __init__(self, gene, data):
         # always set data, unless running in tests
 
         self.gene = gene
-        self.fitness = 0
+        self.fitness = float('-inf')
         self.data = data
-        if data:
-            # possible enhancement: filtered = itertools.compress(s, b)
-            self.weight_sum = sum([self.gene[i] * self.data['weights'][i]
-                                   for i in range(len(self.gene))])
+        self.weight_sum = sum(itertools.compress(data['weights'], self.gene))
 
     def __setitem__(self, key, value):
         self.gene[key] = value
@@ -81,6 +79,14 @@ class Chromosome:
         child1 = x[:point] + y[point:]
         child2 = y[:point] + x[point:]
 
+        # Alternative crossover: choose random bits and swap 'em
+        # child1 = x[:]
+        # child2 = y[:]
+        # k = random.randint(data['n']//4, data['n']//2)
+        # genes_to_swap = random.sample(range(data['n']), k)
+        # for i in genes_to_swap:
+        #     child1[i], child2[i] = child2[i], child1[i]
+
         return Chromosome(child1, data), Chromosome(child2, data)
 
     def mutate(self):
@@ -89,7 +95,9 @@ class Chromosome:
         """
 
         idx = random.randint(0, len(self.gene) - 1)
+        self.weight_sum -= self.data['weights'][idx] * self.gene[idx]
         self.gene[idx] = 1 - self.gene[idx]
+        self.weight_sum += self.data['weights'][idx] * self.gene[idx]
 
     def get_fitness(self, has_penalty=True):
         if self.fitness <= 0:
@@ -116,8 +124,17 @@ class Chromosome:
                 sum_diff = self.weight_sum - self.data['capacity']
 
                 # Set penalty
-                penalty = np.log2(1 + p * sum_diff)
+                # penalty = np.log2(1 + p * sum_diff) <- fails
+                penalty = p * sum_diff
 
         # Find fitness = sum(x[i] * P[i] - Penalty(x))
-        return sum([self.gene[i] * self.data['profits'][i] for i in range(len(self.gene))]) - penalty
+        self.fitness = sum(itertools.compress(self.data['profits'], self.gene)) - penalty
+        return self.fitness
 
+    def is_overfilled(self):
+        return self.weight_sum > self.data['capacity']
+
+    def decode(self):
+        return [(profit, weight) for profit, weight in
+                zip(list(itertools.compress(self.data['profits'], self.gene)),
+                    list(itertools.compress(self.data['weights'], self.gene)))]
