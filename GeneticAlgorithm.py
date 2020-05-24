6399d681613a7eb2c0ee1ccf6397f537dd1640fc
skalermo <skalermo@gmail.com>
2020-05-24 20:56:52 +0200

    Can run different algorithms

--- GeneticAlgorithm.py
+++ GeneticAlgorithm.py
@@ -3,49 +3,60 @@ from datetime import datetime
 from Chromosome import Chromosome
 
 
-def genetic_algorithm(data, pop_size=100, crossover_rate=0.65, mutation_rate=0.05, seed=None):
+def genetic_algorithm(data, method='vanilla', pop_size=100,
+                      crossover_rate=0.65, mutation_rate=0.05, seed=None):
+    assert method in ['vanilla', 'repair', 'penalty']
     # Set seed
     if seed is None:
         seed = datetime.now().microsecond
     random.seed(seed)
 
     # Initialize population
-    population = init_population(pop_size, data)
-
-    progress = population[0].fitness
-    progress_avg = sum(chromo.fitness for chromo in population) / len(population)
-    yield population, progress, progress_avg
+    population = init_population(pop_size, data, method)
 
     while True:
-        population = next_generation(data, population, crossover_rate, mutation_rate)
-        progress = population[0].fitness
-        progress_avg = sum(chromo.fitness for chromo in population) / len(population)
-
-        yield population, progress, progress_avg
+        population = next_generation(population, method, crossover_rate, mutation_rate)
+        yield population
 
 
-def next_generation(data, population, crossover_rate=0.65, mutation_rate=0.05, elite_size=1):
+def next_generation(population, method, crossover_rate=0.65,
+                    mutation_rate=0.05, elite_size=1):
 
     # Select and breed parents
-    parents = selection(population, elite_size, 100) # selection size ?
+    parents = selection(population, elite_size, len(population)//2)
     children = breed(parents, crossover_rate)
 
     # Mutate parents and children
     new_individuals = parents + children
     mutate_chromosomes(new_individuals, mutation_rate)
 
-    # repair_chromosomes(new_individuals)
+    if method == 'repair':
+        repair_chromosomes(new_individuals)
+    if method == 'vanilla':
+        replace_broken_chromosomes(new_individuals)
 
     # Sort chromosomes by their fitness
-    new_population = sorted(new_individuals, key=lambda x: x.get_fitness(True), reverse=True)
+    new_population = sorted(new_individuals,
+                            key=lambda x: x.calc_fitness(method == 'penalty'),
+                            reverse=True)
 
     # Truncate new population size and return new population
     return new_population[:len(population)]
 
 
 def repair_chromosomes(chromosomes):
-    for chromosome in chromosomes:
-        chromosome.repair()
+    for i, chromosome in enumerate(chromosomes):
+        chromosome.repair('greedy')
+        chromosomes[i] = chromosome
+
+
+def replace_broken_chromosomes(chromosomes):
+    for i, chromosome in enumerate(chromosomes):
+        if chromosome.is_overfilled():
+            chromosome = gen_random_chromosome(chromosome.data)
+            chromosome.repair('random')
+            chromosome.calc_fitness(False)
+            chromosomes[i] = chromosome
 
 
 def selection(population, elite_size, selection_size):
@@ -78,9 +89,10 @@ def breed(mating_pool, crossover_rate):
 
     # breed new and add them to children
     for i in range(len(mating_pool)//2):
+        born_children = (mating_pool[i], mating_pool[-i - 1],)
         if random.random() < crossover_rate:
             born_children = Chromosome.crossover((mating_pool[i], mating_pool[-i - 1],))
-            children.extend(born_children)
+        children.extend(born_children)
 
     return children
 
@@ -91,12 +103,13 @@ def mutate_chromosomes(chromosomes, mutation_rate):
             chromosome.mutate()
 
 
-def init_population(pop_size, data):
+def init_population(pop_size, data, method):
     """
     Populate the initial population
     with random solutions.
     :param pop_size: Size of population
     :param data: Input data
+    :param method: Method used in algorithm
     :return: Initialized population
     """
 
@@ -107,8 +120,9 @@ def init_population(pop_size, data):
         population.append(gen_random_chromosome(data))
 
     for chromo in population:
-        chromo.repair(mode='random')
-        chromo.get_fitness()
+        if method != 'penalty':
+            chromo.repair(mode='random')
+        chromo.calc_fitness(method == 'penalty')
 
     return population
 
