4f594796b4dfae0d9c81f3e8abc93b12822036cc
skalermo <skalermo@gmail.com>
2020-05-25 09:13:43 +0200

    Fix tests (once again)

--- tests/test_Mutation.py
+++ tests/test_Mutation.py
@@ -10,18 +10,19 @@ class TestMutations(unittest.TestCase):
     def test_mutate(self):
         chromosome = Chromosome([0, 1, 1, 1, 1, 0, 0, 0], self.data)
         chromosome_mutated = chromosome.copy()
-        chromosome_mutated.mutate()
+        chromosome_mutated.mutate(5)
+        self.assertEqual([0, 1, 1, 1, 1, 1, 0, 0], chromosome_mutated.gene)
         self.assertNotEqual(chromosome.gene, chromosome_mutated.gene)
 
         chromosome = Chromosome([1]*10, self.data)
         chromosome_mutated = chromosome.copy()
-        chromosome_mutated.mutate()
+        chromosome_mutated.mutate(3)
 
         self.assertFalse(all(chromosome_mutated.gene))
 
         chromosome = Chromosome([0]*10, self.data)
         chromosome_mutated = chromosome.copy()
-        chromosome_mutated.mutate()
+        chromosome_mutated.mutate(8)
         self.assertTrue(any(chromosome_mutated.gene))
 
 
--- tests/test_Selection.py
+++ tests/test_Selection.py
@@ -6,7 +6,7 @@ from GeneticAlgorithm import init_population, selection
 class TestSelection(unittest.TestCase):
     def test_selection_output_size(self):
         data = gen_data()
-        population = init_population(50, data)
+        population = init_population(50, data, 'vanilla')
         selected = selection(population, 0, 50)
         self.assertEqual(50, len(selected))
 
